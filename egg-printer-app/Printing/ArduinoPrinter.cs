c9bb0b62eadeab38690f200166184318bfd7cc5f
yurko7 <yurko7@hotmail.com>
2017-07-21 00:02:21 +0300

    Add Printer implementation that prints to egg-printer-driver.

---
+++ egg-printer-app/Printing/ArduinoPrinter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using ArduinoDriver.SerialProtocol;
+using Point = System.Drawing.Point;
+using PrinterPoint = ArduinoDriver.SerialProtocol.Point;
+
+namespace YuKu.EggPrinter.Printing
+{
+    public sealed class ArduinoPrinter : Printer
+    {
+        public ArduinoPrinter(ArduinoDriver.ArduinoDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public override Task PenUp()
+        {
+            var request = new PenRequest(PenState.Up);
+            return _driver.SendAsync(request);
+        }
+
+        public override Task PenDown()
+        {
+            var request = new PenRequest(PenState.Down);
+            return _driver.SendAsync(request);
+        }
+
+        public override Task Move(Point point)
+        {
+            var request = new MoveRequest(ToPrinterPoint(point));
+            return _driver.SendAsync(request);
+        }
+
+        public override Task Dot(Point point)
+        {
+            var request = new DotRequest(ToPrinterPoint(point));
+            return _driver.SendAsync(request);
+        }
+
+        public override Task Line(Point from, Point to)
+        {
+            var request = new LineRequest(ToPrinterPoint(from), ToPrinterPoint(to));
+            return _driver.SendAsync(request);
+        }
+
+        protected override Task BeginPrint()
+        {
+            return _driver.SendAsync(new BeginRequest());
+        }
+
+        protected override Task EndPrint()
+        {
+            return _driver.SendAsync(new EndRequest());
+        }
+
+        private PrinterPoint ToPrinterPoint(Point point)
+        {
+            var printerPoint = new PrinterPoint((Int16) point.X, (Int16) point.Y);
+            return printerPoint;
+        }
+
+        private readonly ArduinoDriver.ArduinoDriver _driver;
+    }
+}
